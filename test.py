ef8e2cbd3b484447eaec3ca1e3584edd33c0c9c9
marcb1387 <marcb1387@gmail.com>
2023-02-09 15:38:06 -0500

    Update test.py
    it has been done

--- test.py
+++ test.py
@@ -11,24 +11,42 @@ def parse_xml(file_path):
 
 # create the table header
 def create_header(frame):
-    tk.Label(frame, text="Car", bg="grey", font=("Arial", 12), width=20, relief="solid").grid(row=0, column=1)
-    tk.Label(frame, text="Number", bg="grey", font=("Arial", 12), width=20, relief="solid").grid(row=0, column=2)
-    tk.Label(frame, text="Result", bg="grey", font=("Arial", 12), width=20, relief="solid").grid(row=0, column=3)
-    tk.Label(frame, text="ElapsedTime", bg="grey", font=("Arial", 12), width=20, relief="solid").grid(row=0, column=4)
-
-  
+    tk.Label(frame, text="Heat", bg="grey", font=("Arial", 12), width=20, relief="solid").grid(row=0, column=1)
+    tk.Label(frame, text="Race", bg="grey", font=("Arial", 12), width=20, relief="solid").grid(row=0, column=2)
+    tk.Label(frame, text="Lane Number", bg="grey", font=("Arial", 12), width=20, relief="solid").grid(row=0, column=3)
+    tk.Label(frame, text="Car", bg="grey", font=("Arial", 12), width=20, relief="solid").grid(row=0, column=4)
+    tk.Label(frame, text="Result", bg="grey", font=("Arial", 12), width=20, relief="solid").grid(row=0, column=5)
+    tk.Label(frame, text="ElapsedTime", bg="grey", font=("Arial", 12), width=20, relief="solid").grid(row=0, column=6)
+    
 
 # create the table body
 def create_body(root, frame):
-    print ("im doing something")
+    races = []
+    heat_count = 0
+    for heat in root.findall('./Heats/Heat'):
+        heat_count += 1
+        for race in heat.findall('./Races/Race'):
+            race_number = race.attrib["Number"]
+            race_lanes = []
+            for lane in race.findall('./Lanes/Lane'):
+                lane_info = {"Car": lane.attrib["Car"], "Number": lane.attrib["Number"],
+                             "Result": lane.attrib["Result"], "ElapsedTime": lane.attrib["ElapsedTime"]}
+                race_lanes.append(lane_info)
+            races.append({"Number": race_number, "Lanes": race_lanes})
+        
+    last_race = races[-1]
     row_index = 1
-    for race in root.findall('./Heats/Heat/Races/Race'):
-        for lane in race.findall('./Lanes/Lane'):
-            tk.Label(frame, text=lane.attrib["Car"], font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=1)
-            tk.Label(frame, text=lane.attrib["Number"], font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=2)
-            tk.Label(frame, text=lane.attrib["Result"], font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=3)
-            tk.Label(frame, text=lane.attrib["ElapsedTime"], font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=4)
-            row_index += 1
+
+    for lane in last_race["Lanes"]:
+        tk.Label(frame, text=heat_count, font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=1)
+        tk.Label(frame, text=last_race["Number"], font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=2)
+        tk.Label(frame, text=lane["Number"], font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=3)
+        tk.Label(frame, text=lane["Car"], font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=4)
+        tk.Label(frame, text=lane["Result"], font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=5)
+        tk.Label(frame, text=lane["ElapsedTime"], font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=6)
+        row_index += 1
+
+
 
 # refresh the table
 def refresh_table(file_path, root, frame):
