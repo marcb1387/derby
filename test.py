3ec2edf426109e11db4d07e678655469137fc943
marcb1387 <marcb1387@gmail.com>
2023-02-09 14:46:28 -0500

    Update test.py
    its now lists everything

--- test.py
+++ test.py
@@ -8,29 +8,35 @@ def parse_xml(file_path):
     tree = ET.parse(file_path)
     return tree.getroot()
 
+
 # create the table header
-def create_header(root, frame):
+def create_header(frame):
     tk.Label(frame, text="Car", bg="grey", font=("Arial", 12), width=20, relief="solid").grid(row=0, column=1)
     tk.Label(frame, text="Number", bg="grey", font=("Arial", 12), width=20, relief="solid").grid(row=0, column=2)
     tk.Label(frame, text="Result", bg="grey", font=("Arial", 12), width=20, relief="solid").grid(row=0, column=3)
     tk.Label(frame, text="ElapsedTime", bg="grey", font=("Arial", 12), width=20, relief="solid").grid(row=0, column=4)
 
+  
+
 # create the table body
 def create_body(root, frame):
+    print ("im doing something")
     row_index = 1
-    for lane in root.findall('Lane'):
-        tk.Label(frame, text=lane.get["Car"], font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=1)
-        tk.Label(frame, text=lane.get["Number"], font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=2)
-        tk.Label(frame, text=lane.get["Result"], font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=3)
-        tk.Label(frame, text=lane.get["ElapsedTime"], font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=4)
-        row_index += 1
+    for race in root.findall('./Heats/Heat/Races/Race'):
+        for lane in race.findall('./Lanes/Lane'):
+            tk.Label(frame, text=lane.attrib["Car"], font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=1)
+            tk.Label(frame, text=lane.attrib["Number"], font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=2)
+            tk.Label(frame, text=lane.attrib["Result"], font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=3)
+            tk.Label(frame, text=lane.attrib["ElapsedTime"], font=("Arial", 12), width=20, relief="solid").grid(row=row_index, column=4)
+            row_index += 1
 
 # refresh the table
 def refresh_table(file_path, root, frame):
     root = parse_xml(file_path)
+    print (root)
     for widget in frame.winfo_children():
         widget.destroy()
-    create_header(root, frame)
+    create_header(frame)
     create_body(root, frame)
     frame.after(5000, refresh_table, file_path, root, frame)
 
@@ -42,7 +48,7 @@ def choose_file():
     root = parse_xml(file_path)
     frame = tk.Frame(root_window)
     frame.pack()
-    create_header(root, frame)
+    create_header(frame)
     create_body(root, frame)
     refresh_table(file_path, root, frame)
 
